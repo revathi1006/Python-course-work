d1cc45bf1e4a2a71819242242a8cd4d113cf14d5
Durgam Revathi <durgamrevathi41@gmail.com>
2025-07-07 15:02:13 +0530

    operators

---
+++ Class work/4.1.PythonOperators.py
@@ -0,0 +1,10 @@
+#1.Arthmentic operators
+a = 10
+b = 20
+print('Addition(a+b):',a+b)
+print('Subtraction(a-b):',a-b)
+print('Multiplication(a*b):',a*b)
+print('Division(a/b):',a/b)
+print('Floor Division(a//b):',a//b)
+print('Modulus(a%b):',a%b)
+print('Exponentiation(a**b):',a**b)
