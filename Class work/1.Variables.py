e5cce18f3e7c341475c78c3eb2bc9f8736e24372
Durgam Revathi <durgamrevathi41@gmail.com>
2025-07-13 16:25:46 +0530

    variables

---
+++ Class work/1.Variables.py
@@ -0,0 +1,28 @@
+#1.Variables 
+#1.1 variables Assignment
+product_name = "Laptop"
+price = 30000
+in_stock = True
+print(product_name,price,in_stock) #Laptop 30000 True
+
+#1.2.Multiple Assignment
+a,b,c = 10,20,30
+print(a,b,c) #10 20 30
+#same value to multiple variable
+x=y=z=100
+print(x,y,z) #100 100 100
+
+#1.3.Reassignment
+x = 10
+y = 20
+print(y) #20
+
+#1.4.swapping Variable
+a,b = 5,10
+a,b = b,a
+print(a,b) #10 5
+
+#1.5.Deleting Variables
+y = 150
+print(y) #150
+del y 
