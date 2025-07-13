7a840cd5e641b6296c2ae07a46eca3a3c9bdfdd5
Durgam Revathi <durgamrevathi41@gmail.com>
2025-07-13 16:32:54 +0530

    datatype

---
+++ Class work/2.Datatype.py
@@ -0,0 +1,15 @@
+#2.Data Type
+#1.1.int
+a = 20
+print(a) #20
+print(type(a)) #<class 'int'>
+
+#1.2.float
+b = 2.5
+print(b) #2.5
+print(type(b)) #<class 'int'>
+
+#1.3.complex
+c = 4+5j
+print(c) #(4+5j)
+print(type(c)) #<class 'complex'>i
